02b41f10afccc8a513485ae1bfd1ae8c6742a1ef
VohobjonovSadorbek <vohobjonovsardorbek2005@gmail.com>
2026-08-24 15:54:35 +0500

    some changed

--- arion_export/urls.py
+++ arion_export/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('api/', include('payments.urls')),
     # API Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
